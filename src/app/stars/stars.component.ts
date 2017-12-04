dd6500e7a0a58b142b721aa4b1eac58721e45bc3
zhangsanfeng <287955197@qq.com>
2017-12-04 19:15:27 +0800

    增加样式绑定

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -15,17 +15,8 @@ export class StarsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const full: number = Math.floor(this.rating);
-    const half: number = Math.ceil(this.rating - full);
-    const empty: number = 5 - full - half;
     for (let i = 0; i < 5; i++) {
-      if (i < full) {
-        this.stars.push('full');
-      } else if (i === full && half !== 0) {
-        this.stars.push('half');
-      } else {
-        this.stars.push('empty');
-      }
+        this.stars.push(i > this.rating);
     }
   }
 
